acb2e14e400d7ac30a0e522d998911b347e6898a
flandish <frankconway1@gmail.com>
2023-02-20 22:21:07 +0000

    [Snapshot for documentation] Got vectors making lines connected one after the other. I don't think I would have got here if I didn't simplify without vectors and go down the vertex and classical for-loop structure.

--- sketch.js
+++ sketch.js
@@ -7,23 +7,20 @@ function setup() {
   background(190);
   strokeWeight(10);
   
-  // let roofSpine = Array.from({length: 5},  () => new Object({
-  //   "x":randomNumber(windowWidth),
-  //   "y":randomNumber(windowHeight)
-  // }));
-
-  let roofSpine = Array.from({length: 5}, () => new Object({
-    "x":randomNumber(windowWidth)
-    //constrain(randomNumber(),prevVertex, max)
-    ,"y":randomNumber(windowHeight)
-  }));
+  let roofSpine = Array.from({length: 7}, () => p5.Vector.random2D());
 
   for (let i=1; i<roofSpine.length; i++){
     let prevVert = roofSpine[i-1];
+    push()
+    translate(width/2, height/2)
     beginShape(LINES);
-      vertex(prevVert.x, prevVert.y);
-      vertex(roofSpine[i].x, roofSpine[i].y);
+      // vertex(prevVert.x, prevVert.y);
+      // vertex(roofSpine[i].x, roofSpine[i].y);
+      roofSpine[i].mult(120)
+      vertex(prevVert.x, prevVert.y)
+      vertex(roofSpine[i].x, roofSpine[i].y)
     endShape();
+    pop()
   }
 
   noLoop();
