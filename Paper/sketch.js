25f5b8bfe55576f2fa3458ddee7aebcae8d47572
flandish <frankconway1@gmail.com>
2023-04-01 23:19:11 +0100

    Using a offset line library for Paper achieves what I am after, but I am quite disapointed I wasn't able to figure out how to solve the problem manually after all this time trying. It works though, but just a bit of a let down. This will be for clients though, so results is all they are after I guess. I wonder if I should be puting time in to something that will be more passive income focused...

--- Paper/sketch.js
+++ Paper/sketch.js
@@ -2,8 +2,14 @@
 //[] Learn how to buld a path
 //[] learn how to work with angles
 
+// let canvas = document.querySelector('myCanvas')
 
 //Build a path from randomly scattered points
+let rw = Math.random()*view.size.width;
+let rh = Math.random()*view.size.height;
+
+
+
 let building = {
     chunk: 30,
     lineThick: 5,
@@ -15,22 +21,82 @@ let building = {
     innerPoles: []
 }
 
-let spine = new Path({
 
+
+console.log()
+
+strokeWidth = 10
+
+let spine = new Path({
+    segments: [[50,50], [100,100], [200, 100], [220,200]],
+    strokeColor: 'black',
+    strokeWidth: 10,
+    strokeCap: 'round',
+    strokeJoin: 'round'
 })
 
-let myCircle = new Path.Circle({   
-    center: view.center,
-    radius: 50,
-    fillColor:'black',
-    selected: true
-});
 
-function onMouseDown(event){
-    myCircle.position = event.point
-    myCircle.radius = 20
-    console.log(event.point)
-}
+//random points where each X is larger than the last
+let vec1 = spine.segments[1].point - spine.segments[0].point
+let vec2 = spine.segments[2].point - spine.segments[1].point
+
+
+
+// let pole = new Path()
+// let p = spine.segments[1].point
+// let q = spine.segments[2].point
+
+// let joe = new Point(p+20)
+// joe.angle = 10
+
+// pole.add(p,joe)
+let chunk = 30
+
+let hello = PaperOffset.offset(spine, chunk)
+let goodbye = PaperOffset.offset(spine, -chunk)
+
+let pole = new Path({
+    segments: [hello.segments[1], goodbye.segments[2]],
+    strokeWidth: 10,
+    strokeColor: 'black'
+}) //maybe: connect all odd indexes of hello with all even of goodbye
+
+// pole.add(hello.segments[1], goodbye.segments[2])
+
+// let qole = new Path()
+// qole.add(q, q+20)
+// qole.add(q, q+20)
+// qole.segments[1].angle = 90
+
+// let p1 = new Point([0,0], [50,50])
+// let p2 = new Point([0,0, [100,100]])
+
+// let vec = p2-p1
+
+
+
+
+
+
+
+let j = new Point(vec1.getDirectedAngle(vec2))
+console.log(
+    vec1.getDirectedAngle(vec2)
+)
+
+
+
+//some things to play with
+// Point.random()
+// Point.angle
+// getAngle(point)
+// getDirectedAngle(point)
+
+//create Points with set limitatons on .angle to avoid sharper corners
+
+// console.log(spine.segemnts[0])
+
+project.activeLayer.selected = true;
 
 function onKeyUp(event){
     if(event.key == 'd'){
@@ -39,5 +105,18 @@ function onKeyUp(event){
 }
 
 // console.log("moo")
+//let myCircle = new Path.Circle({   
+    //     center: view.center,
+    //     radius: 50,
+    //     fillColor:'black',
+    //     selected: false
+    // });
+    
+    // function onMouseDrag(event){
+    //     if(myCircle.hitTest(event.point)){
+    //     myCircle.position = event.point
+    //     }
+    //     myCircle.radius = 20
+    //     console.log(event.point)
+    // }
 
-// let hey = Math.random()*view.size.width;
